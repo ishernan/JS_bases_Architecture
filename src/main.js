bfb60b00968b87087e59d177b39e9e205be461c3
ishernan <hernandez.isaac@gmail.com>
2021-12-03 11:28:59 +0100

    add setTimeOut

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    
+    setTimeout(()=>{
+        document.querySelector('div.outer-splash').classList.add('hidden')
+    }, 2000)
 });
 
 
